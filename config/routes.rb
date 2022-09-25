7a357164f3f2987b6b2c12d7b8d5b6cd55853e2b
pramodd12321 <66937620+pramodd12321@users.noreply.github.com>
2022-09-25 18:49:11 +0545

    Add files via upload

---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  resources :posts do
+    resources :comments
+    
+    
+  end
+
+  root "posts#index"
+  
+
+  
+end
+
+
+
+
+
---
+++ db/migrate/20220921032650_add_post_id_to_comments.rb
@@ -0,0 +1,7 @@
+class AddPostIdToComments < ActiveRecord::Migration[7.0]
+  def change
+
+    add_column :comments, :post_id, :integer
+
+  end
+end
